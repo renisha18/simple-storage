04e76f03b0cec6e7168599c528e3713951393b17
star <renishaensalaita.27it@licet.ac.in>
2026-01-15 00:15:43 +0530

    depoy.js changes

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ import fs from 'fs-extra';
 
 async function main(){
 //http://127.0.0.1:7545
-const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
 const wallet = new ethers.Wallet(
     "0x42ee8d37aede62887a74d64a85f1842d24d2d336800abf68d6f20e60d50cfcfd",
     provider
@@ -19,7 +19,9 @@ const binary = fs.readFileSync(
 const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
 console.log("Deploying please wait");
 const contract = await contractFactory.deploy();
-console.log(contract);
+const transactionReceipt = await contract.deployTransaction.wait(1);
+console.log(transactionReceipt);
+
 }
 
 main()
