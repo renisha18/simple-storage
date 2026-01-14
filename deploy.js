69795484108fb7f5866358f569303a6babd931ec
star <renishaensalaita.27it@licet.ac.in>
2026-01-14 11:16:34 +0530

    ether setup

--- deploy.js
+++ deploy.js
@@ -1,5 +1,25 @@
-async function main(){
+import { ethers } from 'ethers';
+import fs from 'fs-extra';
 
+async function main(){
+//http://127.0.0.1:7545
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+const wallet = new ethers.Wallet(
+    "0x42ee8d37aede62887a74d64a85f1842d24d2d336800abf68d6f20e60d50cfcfd",
+    provider
+);
+const abi = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.abi",
+    "utf8"
+);
+const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+);
+const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
+console.log("Deploying please wait");
+const contract = await contractFactory.deploy();
+console.log(contract);
 }
 
 main()
